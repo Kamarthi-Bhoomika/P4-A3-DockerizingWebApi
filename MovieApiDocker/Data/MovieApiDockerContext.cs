8b8cf766140a7abcdcea504797fbe2cca45114cb
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-16 14:10:24 +0530

    initial commit

---
+++ MovieApiDocker/Data/MovieApiDockerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApiDocker.Models;
+
+namespace MovieApiDocker.Data
+{
+    public class MovieApiDockerContext : DbContext
+    {
+        public MovieApiDockerContext (DbContextOptions<MovieApiDockerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApiDocker.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieApiDocker/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApiDocker.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
